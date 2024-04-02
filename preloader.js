d378154767fa6eb3d04ab2e79d652217360a90d5
ls <lucas.jona.silva@gmail.com>
2024-04-02 01:13:17 -0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -16,7 +16,7 @@ overlay.style.cssText = `
 
 const svgImage = document.createElement('img');
 svgImage.id = 'svgImage';
-svgImage.src = 'https://jvsc99.github.io/preloaderJs/imagem.svg';
+svgImage.src = 'https://alfa-13.github.io/plf/logo-app.png';
 svgImage.style.cssText = `
     max-width: 100%;
     max-height: 100%;
