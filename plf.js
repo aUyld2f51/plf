ab61067fc3c2c71254d089defc6d3ba28e418049
ls <lucas.jona.silva@gmail.com>
2024-04-16 14:11:20 -0300

    Update plf.js

--- plf.js
+++ plf.js
@@ -1,14 +1,17 @@
-document.querySelector('body').style.cssText = `
+const overlay = document.createElement('div');
+overlay.style.cssText = `
     width: 100%;
     height: 100%;
 
     display: flex;
     justify-content: center;
     align-items: center;
-    
+
     background-image: url('https://alfa-13.github.io/plf/lg.svg');
     background-color: #fff;
     background-size: 50%;
     background-repeat: no-repeat;
     background-position: center center;
 `;
+
+document.body.appendChild(overlay);
