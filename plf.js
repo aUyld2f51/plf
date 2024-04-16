d3ad7f1cf6df3d4bf89532f73bddebe226e57d2a
ls <lucas.jona.silva@gmail.com>
2024-04-16 14:05:07 -0300

    Update plf.js

--- plf.js
+++ plf.js
@@ -1,11 +1,4 @@
 document.querySelector('body').style.cssText = `
-    width: 100%;
-    height: 100%;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
     background-image: url('https://alfa-13.github.io/plf/svg.png');
     background-color: #fff;
     background-size: 50%;
