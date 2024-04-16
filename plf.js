45a80e02eb9d69f939fbfc302ab84f8c8611a92e
ls <lucas.jona.silva@gmail.com>
2024-04-16 12:53:09 -0300

    Update plf.js

--- plf.js
+++ plf.js
@@ -1,35 +1,20 @@
-// Create the overlay and SVG elements
-const overlay = document.createElement('div');
-overlay.id = 'loader';
-overlay.style.cssText = `
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: red;
-    z-index: 999;
-`;
+document.body.style.ccsText = `
+    background-color: blue;
+`
 
 const svgImage = document.createElement('img');
 svgImage.id = 'svgImage';
 svgImage.src = 'https://alfa-13.github.io/plf/lg.png';
 svgImage.style.cssText = `
-    background-color: blue;
+    display: block;
+    background-color: red;
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
 `;
-svgImage.style.display = 'block';
 
-// Append the elements to the body
-document.body.appendChild(overlay);
 document.body.appendChild(svgImage);
-
 
 function hideOverlay() {
     svgImage.style.display = 'block';
