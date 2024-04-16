c058a665100d50d45a5ea3cb08d197b84f5b7eb7
ls <lucas.jona.silva@gmail.com>
2024-04-16 13:27:05 -0300

    Update plf.js

--- plf.js
+++ plf.js
@@ -1,30 +1,26 @@
 const overlay = document.createElement('div');
 overlay.id = 'loader';
 overlay.style.cssText = `
+    width: 100%;
+    height: 100%;
+
     display: flex;
     justify-content: center;
     align-items: center;
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    z-index: 999;
 
     background-image: url('https://alfa-13.github.io/plf/dldt.svg');
-    background-color: #3CC7F8;
-    background-size: 25%;
+    background-color: #2387E9;
+    background-size: 20%;
     background-repeat: no-repeat;
     background-position: center center;
 `;
 
 document.body.appendChild(overlay);
 
-
 function hideOverlay() {
     overlay.style.display = 'none';
 }
 
 window.addEventListener('load', () => {
-    setTimeout(hideOverlay, 2000);
+    setTimeout(hideOverlay, 5000);
 });
